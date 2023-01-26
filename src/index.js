fc67916ce0a149d9d709de33e8a18664f8d69684
rodion006 <rodik007zhodino@gmail.com>
2023-01-26 22:46:58 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(nums) {
+    let string = '';
+    nums = nums.toString();
+    let i = 1;
+    while (i < (nums.length + 1)) {
+        string = string + nums[nums.length - i];
+        i++;
+    }
+
+    if (string[string.length - 1] === '-') {
+        let string1 = ''
+        i = 0;
+        while (i < nums.length - 1) {
+            string1 = string1 + string[i];
+            i++;
+        }
+        return +string1;
+    } else {
+        return +string;
+    }
+
 }
